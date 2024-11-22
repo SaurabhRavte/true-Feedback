5f7a2afe2c8dd4eadfc8f1006cd059750360c9a8
Saurabh Ravte <saurabh.ravte@gmail.com>
2024-11-22 11:16:07 +0530

    create Navbar and message card

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,19 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 
-import AuthProvider from "@/context/AuthProvider";
-import { Toaster } from "@/components/ui/toaster";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import Navbar from "@/components/Navbar";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -27,14 +15,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <AuthProvider>
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
-          {children}
-          <Toaster />
-        </body>
-      </AuthProvider>
+      <Navbar />
+      {children}
     </html>
   );
 }
